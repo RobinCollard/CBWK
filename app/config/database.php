f161dd905ea82e9829610c55ca9a716346023e5c
michael van de Ven <michael.vd.ven@hotmail.com>
2014-03-19 13:39:52 +0100

    db nam echange

--- app/config/database.php
+++ app/config/database.php
@@ -54,10 +54,10 @@ return array(
 
 		'mysql' => array(
 			'driver'    => 'mysql',
-			'host'      => '212.83.232.237:3306',
-			'database'  => 'Worldmap',
-			'username'  => 'confusingbucket',
-			'password'  => 'bucket007',
+			'host'      => 'mysql.hostfree.nl',
+			'database'  => 'u199057476_A',
+			'username'  => 'u199057476_root',
+			'password'  => '123456',
 			'charset'   => 'utf8',
 			'collation' => 'utf8_unicode_ci',
 			'prefix'    => '',
